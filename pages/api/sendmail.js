f535f0f1626c9c1af7832231f584c256e04b0d93
Rexedge <ap.oyeniran@gmail.com>
2023-06-03 07:37:08 +0100

    acknowledgement email

--- pages/api/sendmail.js
+++ pages/api/sendmail.js
@@ -1,41 +1,67 @@
-import nodemailer from "nodemailer"
+import nodemailer from 'nodemailer';
 
 export default async function ContactAPI(req, res) {
-	const user = process.env.GMAIL_USER
-	const { name, email, message, subject } = req.body
+	const user = process.env.GMAIL_USER;
+	const { name, email, message, subject } = req.body;
 
 	const data = {
-		name, email, message, subject
-	}
+		name,
+		email,
+		message,
+		subject,
+	};
+	console.log(data);
 	const transporter = nodemailer.createTransport({
-		host: "smtp.zoho.com",
+		host: 'smtp.zoho.com',
 		port: 465,
 		secure: true,
 		auth: {
 			user: user,
 			pass: process.env.GMAIL_PASSWORD,
-		}
+		},
 	});
 
 	try {
+		// Sending the first email to info@smartstepssolution.com
 		const mail = await transporter.sendMail({
 			from: 'info@smartstepssolution.com',
-			to: [email, 'info@smartstepssolution.com'],
-			replyTo: 'info@smartstepssolution.com',
-			subject: `${subject}`,
+			to: 'info@smartstepssolution.com',
+			replyTo: email,
+			subject: subject,
 			html: `
-			<p> Name: ${name} </p>
-			<p> Email: ${email} </p>
-			<p> Message: ${message} </p>
-			`,
-		})
+				<p> Name: ${name} </p>
+				<p> Email: ${email} </p>
+				<p> Message: ${message} </p>
+				`,
+		});
 
-		console.log("Message sent: ", mail.messageId);
-		return res.status(200).json({ message: "success" })
+		console.log('Message sent: ', mail.messageId);
+
+		// Sending the acknowledgment email to the sender
+		const acknowledgmentTemplate = `
+			<p>Dear ${name},</p>
+			<p>Thank you for contacting us. We have received your message and a representative will get back to you shortly.</p>
+			<p>Best regards,</p>
+			<p>The Smart Steps Solution Team</p>
+		`;
+
+		const acknowledgmentMail = await transporter.sendMail({
+			from: 'info@smartstepssolution.com',
+			to: email,
+			replyTo: 'info@smartstepssolution.com',
+			subject: 'Acknowledgment - We have received your message',
+			html: acknowledgmentTemplate,
+		});
+
+		console.log('Acknowledgment email sent:', acknowledgmentMail.messageId);
+
+		return res
+			.status(200)
+			.json({ message: 'success', acknowledgmentMailId: acknowledgmentMail.messageId });
 	} catch (error) {
 		console.log(error);
 		res.status(500).json({
-			message: "Message not sent"
-		})
+			message: 'Message not sent',
+		});
 	}
 }
