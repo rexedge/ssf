128795461ece1b12855adb9f2eeafd3022ffb47b
oyeniran Ayobami Paul <ap.oyeniran@gmail.com>
2024-05-31 09:26:24 +0100

    upd: become an agent

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -2,5 +2,14 @@ import * as z from 'zod';
 
 export const BecomeAnAgentSchema = z.object({
 	email: z.string().email(),
-	name: z.string().min(5, { message: 'Enter valid name' }),
+	fname: z.string().min(5, { message: 'Enter valid name' }),
+	lname: z.string().min(5, { message: 'Enter valid name' }),
+	phone: z.string(),
+	insurance: z.string(),
+	age: z.string(),
+	experience: z.string(),
+	license: z.string(),
+	transportation: z.string(),
+	fullparttime: z.string(),
+	commission: z.string(),
 });
